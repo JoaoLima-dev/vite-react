dd44bd7f4f01a4dee37cbf100e59eeb2fa13c068
Joao <paladino160@hotmail.com>
2023-02-14 20:48:39 +0000

    second commit

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,38 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
-
 export function Comment() {
-return (
-    <div className={styles.comment}></div>
-)
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1680&q=80" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="10-10 at 15:18" dateTime="2022-10-10">
+                About an hour ago
+              </time>
+            </div>
+
+            <button title="Delete Comment">
+                <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>Great Davon, congratulations!! 😁😁😁</p>
+        </div>
+
+        <footer>
+            <button>
+            <ThumbsUp />
+            Like <span>20</span>
+
+            </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -5,9 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1680&q=80"
+          <Avatar src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1680&q=80"
           />
           <div className={styles.authorinfo}>
             <strong>Kath</strong>
@@ -51,6 +51,12 @@ export function Post() {
           <button type="submit"> Post </button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }
